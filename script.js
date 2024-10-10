c7b3b05e36d543c56d1e87566b9e08ab1f9bf94f
fivank <156933336+fivank@users.noreply.github.com>
2024-10-10 12:10:12 +0000

    more robust js file for hosting

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let currentPrompt;
 
 // Load default data.json on page load
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('data.json')
+    fetch('./data.json')
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.json();
         })
